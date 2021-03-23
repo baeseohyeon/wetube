df63de9a8e0ec06302c63e089905d4bfe452ea69
baeseohyeon <formnarago@naver.com>
2021-03-23 20:36:59 +0900

    end

---
+++ routers/videoRouter.js
@@ -0,0 +1,31 @@
+import express from "express";
+import routes from "../routes";
+import {
+  getUpload,
+  postUpload,
+  videoDetail,
+  deleteVideo,
+  getEditVideo,
+  postEditVideo,
+} from "../controllers/videoController";
+import { uploadVideo, onlyPrivate } from "../middlewares";
+const videoRouter = express.Router();
+
+//upload
+
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
+
+//video detail
+
+videoRouter.get(routes.videoDetail(), videoDetail);
+
+// editvideo
+
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
+
+//delete video
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
+
+export default videoRouter;
